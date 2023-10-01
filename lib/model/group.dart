798a714e3578949da8f739b46684fc92be19fdc4
Kenji Sanehira <kenjisan@hawaii.edu>
2023-09-30 14:17:02 -1000

    Made Specific Advancements and detailed groups

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -6,8 +6,7 @@
  *
  */
 
-//TODO: Add a category to sort on
-
+///The data associated with groups.
 class Group {
   Group({
     required this.groupName, // Ex. ICS 466
@@ -24,3 +23,54 @@ class Group {
   int newMessages;
 
 }
+
+///Provides access to and operations on all defined groups.
+class GroupsDB {
+  final List<Group> allGroups = [
+      Group(
+        groupName: 'ICS 466',
+        semYear: 'Fall 2023',
+        professor: 'Philip Johnson',
+        groupMembers: 42,
+        newMessages: 3,
+      ),
+      Group(
+        groupName: 'ICS 332',
+        semYear: 'Fall 2023',
+        professor: 'Henri Casanova',
+        groupMembers: 37,
+        newMessages: 17,
+      ),
+      Group(
+        groupName: 'ICS 312',
+        semYear: 'Fall 2023',
+        professor: 'Kim Binsted',
+        groupMembers: 25,
+        newMessages: 0,
+      ),
+      Group(
+        groupName: 'Math 307',
+        semYear: 'Fall 2023',
+        professor: 'Julian Hachmeister',
+        groupMembers: 42,
+        newMessages: 10,
+      ),
+      Group(
+        groupName: 'ICS 496',
+        semYear: 'Fall 2023',
+        professor: 'Anthony Peruma',
+        groupMembers: 30,
+        newMessages: 2,
+      ),
+    ];
+
+  Group getGroup(String groupName) {
+    return allGroups.firstWhere((group) => group.groupName == groupName);
+  }
+
+  List<Group> getGroups() {
+    return allGroups;
+  }//loadGroups
+} //Groups Repository
+
+GroupsDB groupsDB = GroupsDB();
