f6d2abd6485776ca6db6755df9326876e8e2fb1e
michho8 <mh888@hawaii.edu>
2023-10-04 15:25:39 -1000

    Add fields and empty validation

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:connectuni/screens/signup.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/login.dart';
@@ -13,20 +14,26 @@ import 'screens/search_groups_screen.dart';
 
 class ConnectUniApp extends StatelessWidget {
   ConnectUniApp({Key? key}) : super(key: key);
-  final PageController _pageController = PageController(); // this does nothing for now, I need it here to initialize the routes
+  final PageController _pageController =
+      PageController(); // this does nothing for now, I need it here to initialize the routes
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'ConnectUni',
-      initialRoute: '/',
+      initialRoute: '/signup',
       routes: {
         // TODO: Insert routes
         '/login': (BuildContext context) => const LoginPage(),
-        '/': (BuildContext context) => const HomePage(),
+        '/signup': (BuildContext context) => const SignUpPage(),
+        '/home': (BuildContext context) => const HomePage(),
         '/settings': (BuildContext context) => const Settings(),
-        '/searchgroups': (BuildContext context) => SearchGroupsScreen(pageController: _pageController,),
-        '/searchpeople': (BuildContext context) => SearchPeopleScreen(pageController: _pageController,),
+        '/searchgroups': (BuildContext context) => SearchGroupsScreen(
+              pageController: _pageController,
+            ),
+        '/searchpeople': (BuildContext context) => SearchPeopleScreen(
+              pageController: _pageController,
+            ),
         '/groupinfo': (BuildContext context) => const GroupInfo(),
         '/conversations': (BuildContext context) => const ChatPage(),
         '/profile': (BuildContext context) => const ProfilePage(),
---
+++ lib/screens/signup.dart
@@ -0,0 +1,179 @@
+import 'package:connectuni/screens/profile.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({Key? key}) : super(key: key);
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final _emailController = TextEditingController();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _passwordCheckController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _usernameController.dispose();
+    _passwordController.dispose();
+    _passwordCheckController.dispose();
+    super.dispose();
+  }
+
+  bool _filledFields() {
+    return false;
+  }
+
+  bool _samePassword() {
+    if (_passwordController.value == _passwordCheckController.value) {
+      return true;
+    }
+    return false;
+  }
+
+  bool _emptyField(TextEditingController controllerName) {
+    if (controllerName.value.text.isEmpty) {
+      return false;
+    }
+    return true;
+  }
+  //
+  // String? get _errorMessage {
+  //   final input = controllerName.value.text;
+  //   if (input.isEmpty) {
+  //     return true;
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(
+              horizontal: 50.0), //controls width of textfields
+          children: <Widget>[
+            const SizedBox(height: 80.0),
+            Column(
+              children: <Widget>[
+                Text(
+                  'Sign up for ConnectUni!',
+                  style: Theme.of(context).textTheme.headlineSmall,
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 75.0, //padding from title to textfields
+            ),
+            const Text('Enter your email'),
+            TextField(
+              controller: _emailController,
+              decoration: InputDecoration(
+                  filled: true,
+                  labelText: "Email",
+                  errorText: !_emptyField(_emailController)
+                      ? 'Email can\'t be empty'
+                      : null),
+              onChanged: (text) => setState(() => _emailController.value),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text('Enter your username'),
+            TextField(
+              controller: _usernameController,
+              decoration: InputDecoration(
+                  filled: true,
+                  labelText: "Username",
+                  errorText: !_emptyField(_usernameController)
+                      ? 'Username can\'t be empty'
+                      : null),
+              onChanged: (text) => setState(() => _usernameController.value),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text('Enter your password'),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                filled: true,
+                labelText: "Password",
+                errorText: !_emptyField(_passwordController)
+                    ? 'Password can\'t be empty'
+                    : null,
+              ),
+              onChanged: (text) => setState(() => _passwordController.value),
+              obscureText: true,
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text('Re-type your password'),
+            TextField(
+              controller: _passwordCheckController,
+              //install a listener to compare it to the password value above
+              decoration: InputDecoration(
+                filled: true,
+                labelText: "Password",
+                errorText: !_emptyField(_passwordCheckController)
+                    ? 'Password can\'t be empty'
+                    : null,
+              ),
+              onChanged: (text) =>
+                  setState(() => _passwordCheckController.value),
+              obscureText: true,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 100.0,
+                  width: 50.0,
+                ),
+                //clear button
+                TextButton(
+                  onPressed: () {
+                    _emailController.clear();
+                    _passwordController.clear();
+                    _usernameController.clear();
+                    _passwordCheckController.clear();
+                  },
+                  child: const Text("CLEAR"),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_filledFields()) {
+                      //check if all fields are filled
+                      if (_samePassword()) {
+                        //validate passwords match
+                        Navigator.push(
+                          context,
+                          CupertinoPageRoute(
+                              builder: (context) => const ProfilePage()),
+                        );
+                      } else {
+                        _emailController.text = "not smae passowrd";
+                      }
+                    } else {
+                      _emailController.text = "whaaauhhhh";
+                    }
+
+                    //create new account stuff - pick username, icons, etc.
+                  },
+                  child: const Text('SIGN UP'),
+                ),
+                //next button
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
