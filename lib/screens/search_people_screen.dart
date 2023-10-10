30bbb42d8e90fed8ea20d986175e53b90acd4d3b
Raphael Anroe Bumanlag <89962623+RaphaelBumanlag@users.noreply.github.com>
2023-10-10 11:56:41 -1000

    Search Icons Change

--- lib/screens/search_people_screen.dart
+++ lib/screens/search_people_screen.dart
@@ -26,7 +26,7 @@ class _SearchPeopleScreenState extends State<SearchPeopleScreen> {
         actions: <Widget>[
           IconButton(
             icon: const Icon(
-                Icons.arrow_back_ios,
+                Icons.group,
                 semanticLabel: 'Search for groups',
             ),
             onPressed: () {
@@ -35,7 +35,7 @@ class _SearchPeopleScreenState extends State<SearchPeopleScreen> {
           ),
           IconButton(
             icon: const Icon(
-              Icons.arrow_forward_ios,
+              Icons.celebration,
               semanticLabel: 'Search for events',
             ),
             onPressed: () {
