29df1835622da0296577742824b0d5799f7640da
Kenji Sanehira <kenjisan@hawaii.edu>
2023-10-10 23:45:39 -1000

    Add Functional Calendar

--- lib/screens/eventCalendar.dart
+++ lib/screens/eventCalendar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:multi_select_flutter/multi_select_flutter.dart';
+import 'package:table_calendar/table_calendar.dart';
 
 import '../model/group.dart';
+import '../model/event_db.dart';
 
 /**
  * Implements a Calendar that is able to display events and their details.
@@ -17,6 +19,9 @@ class EventCalendar extends StatefulWidget {
 
 class _EventCalendarState extends State<EventCalendar> {
   final _items = groupsDB.getAllGroups().map((gName) => MultiSelectItem(gName, gName.groupName)).toList();
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
@@ -46,7 +51,7 @@ class _EventCalendarState extends State<EventCalendar> {
                 color: Colors.blue,
               ),
               buttonText: Text(
-                "Filter by:",
+                "Filter events by:",
                 style: TextStyle(
                   color: Colors.blue[800],
                   fontSize: 16,
@@ -56,6 +61,34 @@ class _EventCalendarState extends State<EventCalendar> {
                 // TODO: Filter groups by results
               },
             ),
+          ),
+          TableCalendar(
+              focusedDay: _focusedDay,
+              firstDay: DateTime.utc(2023, 10, 1),
+              lastDay: DateTime.utc(2024, 12, 31),
+              selectedDayPredicate: (day) {
+                return isSameDay(_selectedDay, day);
+              },
+              calendarFormat: _calendarFormat,
+              onFormatChanged: (format) {
+                setState(() {
+                  _calendarFormat = format;
+                });
+              },
+              onDaySelected: (selectedDay, focusedDay) {
+                setState(() {
+                  _selectedDay = selectedDay;
+                  _focusedDay = focusedDay;
+                });
+              },
+               onPageChanged: (focusedDay) {
+                  _focusedDay = focusedDay;
+                },
+                eventLoader: (day) {
+                  //Create a list of days associated with the specified day in the method.
+                  //return groupsDB.getAllGroups().map((gName) => MultiSelectItem(gName, gName.groupName)).toList()
+                  return eventsDB.getAllEvents().where((event) => isSameDay(event.eventDate, day)).toList();
+                },
           ),
         ],
       ),
