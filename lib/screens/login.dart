e170e61fa38e390859e6c66da22183ee37a3ca03
michho8 <mh888@hawaii.edu>
2023-10-11 17:20:09 -1000

    Get rid of back button from the Home page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -91,7 +91,7 @@ class _LoginPageState extends State<LoginPage> {
                   onPressed: () {
                     if (checkValidLogin()) {
                       //would switch to user's home screen
-                      Navigator.push(
+                      Navigator.pushReplacement(
                         context,
                         CupertinoPageRoute(
                             builder: (context) => const HomePage()),
