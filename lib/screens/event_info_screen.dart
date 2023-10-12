913a6b6f7439ae477ce44d67ea86bdb2989290fa
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2023-10-11 23:00:30 -1000

    Updated basic event info page

--- lib/screens/event_info_screen.dart
+++ lib/screens/event_info_screen.dart
@@ -13,15 +13,96 @@ class EventInfoScreen extends StatelessWidget {
     Event thisEvent = eventsDB.getEventById(id);
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Event Info'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 300.0,
+              floating: false,
+              pinned: true,
+              stretch: true,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                collapseMode: CollapseMode.parallax,
+                title: Text(thisEvent.eventName,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    )),
+                background: Image.asset(
+                  thisEvent.eventIcon,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ];
+        },
+        body: ListView(
           children: [
-            Text(thisEvent.eventName,
-                style: Theme.of(context).textTheme.titleLarge),
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(
+                  thisEvent.eventDescription,
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Location: ${thisEvent.eventLocation}',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Date: ${thisEvent.eventDate}',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'Members going: ${thisEvent.userIds.length}',
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    print('Add event to user');
+                  },
+                  child: Container(
+                    padding: const EdgeInsets.all(15),
+                    decoration: BoxDecoration(
+                      color: Colors.blue,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: const Text(
+                      'Join Event',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
