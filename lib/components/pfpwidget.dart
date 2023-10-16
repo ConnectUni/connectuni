ef1d068b27d3e985824d6266cb8b085df2af982d
jialia <92237586+jiahuiliao@users.noreply.github.com>
2023-10-15 14:10:58 -1000

    fix formatting, make groupselector a widget

--- lib/components/pfpwidget.dart
+++ lib/components/pfpwidget.dart
@@ -1,82 +1,36 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
-import '../model/user.dart';
 
 class ProfileWidget extends StatelessWidget {
-  final String image;
-  final User currentUser;
-  final VoidCallback onClicked;
-  final bool isEdit;
 
-  const ProfileWidget({
-    required this.image,
-    required this.currentUser,
-    required this.onClicked,
-    this.isEdit = false,
-    Key? key,
-  }) : super(key: key);
+  final ImageProvider? image;
+  final VoidCallback? onEditIconTap;
+
+  const ProfileWidget({super.key,
+    this.image,
+    this.onEditIconTap,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final color = Theme.of(context).colorScheme.primary;
-
-    return Center(
-      child: Stack(
-        children: [
-          buildImage(),
-          Positioned(
-            bottom: 0,
-            right: 4,
-            child: buildEditIcon(color),
+    return Stack(
+      children: <Widget>[
+        CircleAvatar(
+          backgroundImage: image ?? const AssetImage('assets/default_image.png'), // Provide a default image asset path
+          radius: 60,
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: GestureDetector(
+            onTap: onEditIconTap,
+            child: const CircleAvatar(
+              backgroundColor: Colors.blue,
+              radius: 15,
+              child: Icon(Icons.edit, color: Colors.white, size: 20),
+            ),
           ),
-        ],
-      ),
+        )
+      ],
     );
   }
-
-  Widget buildImage() {
-    final image = AssetImage(currentUser.pfp);
-
-    return ClipOval(
-      child: Material(
-        color: Colors.transparent,
-        child: Ink.image(
-          image: image,
-          fit: BoxFit.cover,
-          width: 128,
-          height: 128,
-          child: InkWell(
-              onTap: onClicked,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget buildEditIcon(Color color) => buildCircle(
-    color: Colors.white,
-    all: 3,
-    child: buildCircle(
-      color: color,
-      all: 8,
-      child: Icon(
-        isEdit ? Icons.add_a_photo : Icons.edit,
-        color: Colors.white,
-        size: 20,
-      ),
-    ),
-  );
-
-  Widget buildCircle({
-    required Widget child,
-    required double all,
-    required Color color,
-  }) =>
-      ClipOval(
-        child: Container(
-          padding: EdgeInsets.all(all),
-          color: color,
-          child: child,
-        ),
-      );
 }
--- lib/screens/createprofile.dart
+++ lib/screens/createprofile.dart
@@ -1,10 +1,9 @@
-import 'dart:ffi';
-import 'dart:io';
+import 'package:connectuni/components/groupselectorwidget.dart';
 import 'package:flutter/material.dart';
 import 'package:connectuni/model/user.dart';
-import 'package:image_picker/image_picker.dart';
 import '../components/interestswidget.dart';
 import '../components/pfpwidget.dart';
+import '../model/userList.dart';
 
 class CreateProfile extends StatefulWidget {
   const CreateProfile({Key? key}) : super(key: key);
@@ -16,7 +15,9 @@ class CreateProfile extends StatefulWidget {
 
 class CreateProfileState extends State<CreateProfile> {
   final _usernameController = TextEditingController();
-  late User currentUser;
+
+  //temporary user to test, need to implement way to be "current" user
+  late User currentUser = usersDB.getUserByID('user-004');
   List<String> interests = [''];
   String? _selectedSemester;
   String? _selectedYear;
@@ -41,34 +42,38 @@ class CreateProfileState extends State<CreateProfile> {
                   children: <Widget>[
                     Column(
                       children: [
-                        SizedBox(height: 20),
+                        const SizedBox(height: 20),
                         ProfileWidget(
-                            currentUser: currentUser,
-                            onClicked: () async{}, image: currentUser.pfp
-                        ),
+                            image: AssetImage(currentUser.pfp),
+                            onEditIconTap: () {
+                              // TODO Implement way to pick image
+                            }),
                       ],
                     ),
-                    SizedBox(height: 20),
+                    const SizedBox(height: 20),
+                    const Text(
+                      'Username',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                     Container(
                       padding: const EdgeInsets.all(10),
                       child: TextField(
                         controller: _usernameController,
                         decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: 'Username',
                           labelStyle: TextStyle(
                             fontSize: 20,
                             fontWeight: FontWeight.bold,
                           ),
                         ),
                         onChanged: (value) {
-                          setState(() {
-                            currentUser.displayName = value;
-                          });
+                          setState(() => currentUser.displayName = value);
                         },
                       ),
                     ),
-                    SizedBox(height: 20),
+                    const SizedBox(height: 20),
                     Column(
                       children: [
                         const Text(
@@ -78,32 +83,10 @@ class CreateProfileState extends State<CreateProfile> {
                             fontWeight: FontWeight.bold,
                           ),
                         ),
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          child: DropdownButtonFormField(
-                            items: <String>[
-                              'Computer Science',
-                              'Finance',
-                              'Liberal Arts',
-                              'Mathematics',
-                              'Graphic Design',
-                              'Business'
-                            ].map((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                            onChanged: (value) {
-                              setState(() {
-                                currentUser.major = value!;
-                              });
-                            },
-                          ),
-                        ),
+                        buildMajorSelector(),
                       ],
                     ),
-                    SizedBox(height: 20),
+                    const SizedBox(height: 20),
                     const Text(
                       'Projected Graduation',
                       style: TextStyle(
@@ -111,60 +94,7 @@ class CreateProfileState extends State<CreateProfile> {
                         fontWeight: FontWeight.bold,
                       ),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: DropdownButtonFormField<String>(
-                              value: _selectedSemester,
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  _selectedSemester = newValue!;
-                                  grad = '$_selectedSemester $_selectedYear';
-                                  currentUser.projectedGraduation = grad;
-                                });
-                              },
-                              items: <String>[
-                                'Fall',
-                                'Spring'
-                              ].map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                          SizedBox(width: 20),
-                          Expanded(
-                            child: DropdownButtonFormField<String>(
-                              value: _selectedYear,
-                              onChanged: (String? newValue) {
-                                setState(() {
-                                  _selectedYear = newValue!;
-                                  grad = '$_selectedSemester $_selectedYear';
-                                  currentUser.projectedGraduation = grad;
-                                });
-                              },
-                              items: <String>[
-                                '2023',
-                                '2024',
-                                '2025',
-                                '2026',
-                                '2027',
-                                '2028'
-                              ].map<DropdownMenuItem<String>>((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                    buildGraduationSelector(),
                     const SizedBox(height: 20),
                     const Text(
                       'Select your Interests',
@@ -174,19 +104,121 @@ class CreateProfileState extends State<CreateProfile> {
                       ),
                     ),
                     const SizedBox(
-                      height: 400,
+                      height: 375,
                       child: InterestsSelector(),
                     ),
                     const SizedBox(height: 20),
+                    const Text(
+                      'Select your Groups',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    ///Currently provides all available groups to
+                    ///select from, need to implement way to select
+                    ///or add just groups relevant to the user?
+                    const GroupSelector(),
+                    const SizedBox(height: 20),
                     ElevatedButton(
                       onPressed: () {
-                        Navigator.pop(context);
+                        ///implement update/save user?
+                        ///then navigate to home screen
+                        Navigator.pushNamed(context, '/home');
                       },
-                      child: const Text('Save'),
+                      child: const Text('Continue'),
                     ),
-                  ]),
+                  ]
+              ),
             ),
           ),
-        ));
+        )
+    );
+  }
+
+
+  Widget buildMajorSelector() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: DropdownButtonFormField(
+        items: <String>[
+          'Computer Science',
+          'Finance',
+          'Liberal Arts',
+          'Mathematics',
+          'Graphic Design',
+          'Business'
+        ].map((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        onChanged: (value) {
+          setState(() {
+            currentUser.major = value!;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget buildGraduationSelector() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Expanded(
+            child: DropdownButtonFormField<String>(
+              value: _selectedSemester,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedSemester = newValue!;
+                  grad = '$_selectedSemester $_selectedYear';
+                  currentUser.projectedGraduation = grad;
+                });
+              },
+              items: <String>[
+                'Fall',
+                'Spring'
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Expanded(
+            child: DropdownButtonFormField<String>(
+              value: _selectedYear,
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedYear = newValue!;
+                  grad = '$_selectedSemester $_selectedYear';
+                  currentUser.projectedGraduation = grad;
+                });
+              },
+              items: <String>[
+                '2023',
+                '2024',
+                '2025',
+                '2026',
+                '2027',
+                '2028'
+              ].map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
