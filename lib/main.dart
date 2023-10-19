5252b7cddb22aac71c5f95c5944f4fefce9cf415
Kenji Sanehira <kenjisan@hawaii.edu>
2023-10-19 11:35:40 -1000

    Riverpod Dependency added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'app.dart';
 
-void main() => runApp(ConnectUniApp());
+void main() => runApp(ProviderScope(child: ConnectUniApp()));
