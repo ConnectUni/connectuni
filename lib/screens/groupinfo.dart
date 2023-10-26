f7d14e7a9bbdf90026f95a2336c400b7a92dc2e8
Kenji Sanehira <kenjisan@hawaii.edu>
2023-10-26 11:43:13 -1000

    Added functionality to the GroupInfo page with Riverpod

--- lib/screens/groupinfo.dart
+++ lib/screens/groupinfo.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:connectuni/model/group.dart';
 import '../model/group_list.dart';
+import '../model/user_list.dart';
 
 /// Information page for a specific group that displays the group members as well as a description of the selected group.
 /// There is an icon at the upper right-hand corner for more statistic-related properties of the group.
@@ -23,6 +24,7 @@ class _GroupInfoState extends ConsumerState<GroupInfo> {
   @override
   Widget build(BuildContext context) {
     final GroupList groupsDB = ref.watch(groupsDBProvider);
+    final String currentUser = ref.watch(currentUserProvider);
     Group groupData = groupsDB.getGroupById(widget.id);
     return Scaffold(
       appBar: AppBar(
@@ -96,14 +98,15 @@ class _GroupInfoState extends ConsumerState<GroupInfo> {
             color: Colors.black,
           ),
           //Display a button to leave the group if the user is in the group.
-          if (groupData.userIDs.contains('user-001'))
+          if (groupData.userIDs.contains(currentUser))
             Padding(
               padding: const EdgeInsets.all(10.0),
               //TODO: Make this button conditional on whether or not the user is in the group.
               child: TextButton(
                 onPressed: () {
-                  //Placeholder currently only removes the first user:
-                  groupData.removeUserId('user-001');
+                  //Remove the user from the group's database. Then Refresh the group's database.
+                  groupData.removeUserId(currentUser);
+                  //TODO: Remove groupId from user.
                   ref.refresh(groupsDBProvider);
                 },
                 style: ButtonStyle(
@@ -115,14 +118,15 @@ class _GroupInfoState extends ConsumerState<GroupInfo> {
               ),
             ),
           //Display a button to join the group if the user is not in the group.
-          if (!groupData.userIDs.contains('user-001'))
+          if (!groupData.userIDs.contains(currentUser))
             Padding(
               padding: const EdgeInsets.all(10.0),
               //TODO: Make this button conditional on whether or not the user is in the group.
               child: TextButton(
                 onPressed: () {
-                  //Placeholder currently only removes the first user:
-                  groupData.addUserId('user-001');
+                  //Add the user from the group's database. Then Refresh the group's database.
+                  groupData.addUserId(currentUser);
+                  //TODO: Add groupId from user.
                   ref.refresh(groupsDBProvider);
                 },
                 style: ButtonStyle(
