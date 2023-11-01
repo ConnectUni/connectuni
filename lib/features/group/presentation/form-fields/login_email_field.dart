701d20940ec6a3ce31e2811175c2259e6dff47ad
michho8 <mh888@hawaii.edu>
2023-11-01 10:07:28 -1000

    Merge conflicts

--- lib/features/group/presentation/form-fields/login_email_field.dart
+++ lib/features/group/presentation/form-fields/login_email_field.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
-
 import 'field_padding.dart';
 
 class LoginEmailField extends StatelessWidget {
