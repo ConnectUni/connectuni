7d185239e9ef4d17b265bf9ecd51add54d45bab2
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2023-11-03 14:32:13 -1000

    Moved event providers respectively
    Moved providers from search events screen to event_providers.dart.

--- lib/features/event/presentation/search_events_screen.dart
+++ lib/features/event/presentation/search_events_screen.dart
@@ -17,33 +17,6 @@ class SearchEventsScreen extends ConsumerStatefulWidget {
   ConsumerState<SearchEventsScreen> createState() => _SearchEventsScreenState();
 }
 
-/*
- * These are the state providers that will be used to check the selected
- * interests and the search query.
- */
-final selectedFiltersProvider = StateProvider<List<String>?>((ref) => []);
-final searchQueryProvider = StateProvider<String?>((ref) => "");
-
-/*
- * This provider will be used to filter the events based on the selected
- * interests and the search query from the providers above.
- */
-final filteredEvents = Provider<List<SingleEvent>?>((ref) {
-  final filters = ref.watch(selectedFiltersProvider);
-  final events = ref.watch(eventsDBProvider).getAllEvents();
-  final query = ref.watch(searchQueryProvider);
-  final suggestions = events.where((event) {
-    if(filters!.isNotEmpty) {
-      return event.eventName.toLowerCase().contains(query!.toLowerCase()) && event.interests.any((interest) => filters.contains(interest));
-    } else {
-      return event.eventName.toLowerCase().contains(query!.toLowerCase());
-    }
-  }).toList();
-
-  return suggestions;
-});
-
-
 class _SearchEventsScreenState extends ConsumerState<SearchEventsScreen> {
   final controller = TextEditingController();
   final _interests = interests
