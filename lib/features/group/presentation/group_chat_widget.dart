d868cdfef0a53d048ca95f8529f2432858aab545
Raphael Anroe Bumanlag <raphael6@hawaii.edu>
2023-11-17 11:56:50 -1000

    updated files that don't need providers
    Instead of calling the provider to access the group from the database, the group is now passed down from the parent widget since we only are referencing a single specific group at a time for these widgets.

--- lib/features/group/presentation/group_chat_widget.dart
+++ lib/features/group/presentation/group_chat_widget.dart
@@ -11,12 +11,12 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 /// It is a clickable widget that takes the user to the group chat page.
 
 class GroupChatWidget extends ConsumerStatefulWidget {
-  String id;
-
-  GroupChatWidget({
+  const GroupChatWidget({
     super.key,
-    required this.id,
+    required this.group,
   });
+
+  final Group group;
 
   @override
   ConsumerState<GroupChatWidget> createState() => _GroupChatWidgetState();
@@ -25,18 +25,15 @@ class GroupChatWidget extends ConsumerStatefulWidget {
 class _GroupChatWidgetState extends ConsumerState<GroupChatWidget> {
   @override
   Widget build(BuildContext context) {
-    final GroupList groupsDB = ref.watch(groupsDBProvider);
-    Group groupData = groupsDB.getGroupById(widget.id);
-
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           CupertinoPageRoute(
-              builder: (context) => GroupChatScreen(id: groupData.groupID)),
+              builder: (context) => GroupChatScreen(id: widget.group.groupID)),
         );
       },
-      child: GroupCardView(id: groupData.groupID),
+      child: GroupCardView(group: widget.group),
     );
   }
 }
--- lib/features/group/presentation/group_info_widget.dart
+++ lib/features/group/presentation/group_info_widget.dart
@@ -1,42 +1,35 @@
 import 'package:connectuni/features/group/presentation/group_card_view.dart';
 import 'package:connectuni/features/group/presentation/groupinfo.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import '../data/group_providers.dart';
 import '../domain/group.dart';
-import '../domain/group_list.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 /// GroupChatWidget is a widget that displays the group chat.
 /// It is a clickable widget that takes the user to the group chat page.
 
-class GroupInfoWidget extends ConsumerStatefulWidget {
-  String id;
-
-  GroupInfoWidget({
+class GroupInfoWidget extends StatefulWidget {
+  const GroupInfoWidget({
     super.key,
-    required this.id,
+    required this.group,
   });
 
+  final Group group;
+
   @override
-  ConsumerState<GroupInfoWidget> createState() => _GroupInfoWidgetState();
+  State<GroupInfoWidget> createState() => _GroupInfoWidgetState();
 }
 
-class _GroupInfoWidgetState extends ConsumerState<GroupInfoWidget> {
+class _GroupInfoWidgetState extends State<GroupInfoWidget> {
   @override
   Widget build(BuildContext context) {
-    final GroupList groupsDB = ref.watch(groupsDBProvider);
-    Group groupData = groupsDB.getGroupById(widget.id);
-
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           CupertinoPageRoute(
-              builder: (context) => GroupInfo(id: groupData.groupID)),
+              builder: (context) => GroupInfo(id: widget.group.groupID)),
         );
       },
-      child: GroupCardView(id: groupData.groupID),
+      child: GroupCardView(group: widget.group),
     );
   }
 }
