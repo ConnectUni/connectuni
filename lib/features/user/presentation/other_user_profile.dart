1d5eea8248dd1e4cd5bff69ee224e1659ef370e8
michho8 <mh888@hawaii.edu>
2023-12-04 15:30:30 -1000

    Leave and Join buttons of other profile dependent on current user

--- lib/features/user/presentation/other_user_profile.dart
+++ lib/features/user/presentation/other_user_profile.dart
@@ -21,16 +21,15 @@ class OtherUserProfile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final AsyncValue<AllData> asyncAllData = ref.watch(allDataProvider);
     return asyncAllData.when(
-      data: (allData) =>
-          _build(
-            context: context,
-            currentUser: allData.currentUser,
-            groups: allData.groups,
-            notifications: allData.notifications,
-            ref: ref,
-          ),
-      loading: () => const CULoading(),
-      error: (e, st) => CUError(e.toString(), st.toString()));
+        data: (allData) => _build(
+              context: context,
+              currentUser: allData.currentUser,
+              groups: allData.groups,
+              notifications: allData.notifications,
+              ref: ref,
+            ),
+        loading: () => const CULoading(),
+        error: (e, st) => CUError(e.toString(), st.toString()));
   }
 
   Widget _build({
@@ -41,8 +40,8 @@ class OtherUserProfile extends ConsumerWidget {
     required WidgetRef ref,
   }) {
     GroupCollection groupCollection = GroupCollection(groups);
-    NotificationCollection notificationCollection = NotificationCollection(notifications);
-
+    NotificationCollection notificationCollection =
+        NotificationCollection(notifications);
 
     bool isFriend() {
       return currentUser.friends.contains(user.uid);
@@ -54,13 +53,13 @@ class OtherUserProfile extends ConsumerWidget {
 
     void updateUsers(User user1, User user2) {
       ref.read(editUserControllerProvider.notifier).updateUser(
-        user: user1,
-        onSuccess: () {},
-      );
+            user: user1,
+            onSuccess: () {},
+          );
       ref.read(editUserControllerProvider.notifier).updateUser(
-        user: user2,
-        onSuccess: () {},
-      );
+            user: user2,
+            onSuccess: () {},
+          );
     }
 
     void sendFriendReq(User sendingUser, User receivingUser) {
@@ -73,36 +72,65 @@ class OtherUserProfile extends ConsumerWidget {
         senderID: sendingUser.uid,
       );
       ref.read(editNotificationControllerProvider.notifier).updateNotification(
-        notification: newNotif,
-        onSuccess: () {},
-      );
+            notification: newNotif,
+            onSuccess: () {},
+          );
       receivingUser.notificationIDs.add(newNotif.notificationID);
       updateUsers(sendingUser, receivingUser);
     }
 
     void removeFriendReq(User sendingUser, User receivingUser) {
-      Notification notif = notificationCollection.getNotificationToFrom(receivingUser.uid, sendingUser.uid);
+      Notification notif = notificationCollection.getNotificationToFrom(
+          receivingUser.uid, sendingUser.uid);
       receivingUser.notificationIDs.remove(notif.notificationID);
       ref.read(editNotificationControllerProvider.notifier).deleteNotification(
-        notification: notif,
-        onSuccess: () {
-          Navigator.pop(context);
-        },
-      );
+            notification: notif,
+            onSuccess: () {
+              Navigator.pop(context);
+            },
+          );
       updateUsers(sendingUser, receivingUser);
+    }
+
+    bool memberOfGroup(String groupId) {
+      if (currentUser.groupIDs.contains(groupId)) {
+        return true;
+      }
+      return false;
+    }
+
+    Widget buildPopupDialog(BuildContext context) {
+      return AlertDialog(
+        title: const Text('Sad to see you leave!'),
+        content: const Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text("You have left the group."),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Close'),
+          ),
+        ],
+      );
     }
 
     /// This displays add/remove friends button
     Widget isFriendIcon() {
       if (isFriend()) {
         return FilledButton(
-           onPressed: () {
-             Navigator.pop(context);
-             currentUser.friends.remove(user.uid);
-             user.friends.remove(currentUser.uid);
-             updateUsers(currentUser, user);
-           },
-           child: const Text('Remove Connection :('),
+          onPressed: () {
+            Navigator.pop(context);
+            currentUser.friends.remove(user.uid);
+            user.friends.remove(currentUser.uid);
+            updateUsers(currentUser, user);
+          },
+          child: const Text('Remove Connection :('),
         );
       } else if (requestPending()) {
         return FilledButton(
@@ -185,7 +213,7 @@ class OtherUserProfile extends ConsumerWidget {
       ),
       isFriendIcon(),
       const Divider(
-        height:7,
+        height: 7,
         thickness: 2,
         indent: 20,
         endIndent: 20,
@@ -207,34 +235,33 @@ class OtherUserProfile extends ConsumerWidget {
                 ),
                 textAlign: TextAlign.left,
               ),
-              if(user.interests.isEmpty)
+              if (user.interests.isEmpty)
                 const Padding(
                   padding: EdgeInsets.all(10.0),
                   child: ListTile(
                     title: Center(
                         child: Text("This User has no Interests.",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold))),
+                            style: TextStyle(fontWeight: FontWeight.bold))),
                     textColor: Colors.white,
                     tileColor: Colors.lightBlue,
                   ),
                 ),
               Column(children: [
                 ...user.interests.map((interest) => Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: ListTile(
-                    title: Center(
-                      child: Text(
-                        interest,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
+                      padding: const EdgeInsets.all(5.0),
+                      child: ListTile(
+                        title: Center(
+                          child: Text(
+                            interest,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                         ),
+                        textColor: Colors.white,
+                        tileColor: FlexColor.deepBlueLightTertiary,
                       ),
-                    ),
-                    textColor: Colors.white,
-                    tileColor: FlexColor.deepBlueLightTertiary,
-                  ),
-                )),
+                    )),
               ]),
             ],
           )),
@@ -249,113 +276,148 @@ class OtherUserProfile extends ConsumerWidget {
 
     /// This displays the user's groups.
     Widget displayGroups() => Container(
-      padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            "Their Courses:",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
-            textAlign: TextAlign.left,
-          ),
-          Column(
+          padding: const EdgeInsets.only(top: 10, left: 5, right: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              ...groupCollection
-                  .getGroups(user.groupIDs)
-                  .map((group) =>
-                  Card(
-                    elevation: 8,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15),
-                      side: BorderSide(
-                        color: Colors.grey.withOpacity(0.2),
-                        width: 1,
-                      ),
-                    ),
-                    child: Column(
-                      children: [
-                        ListTile(
-                          title: Text(group.groupName,
-                              style: const TextStyle(
-                                  fontWeight: FontWeight.bold)),
-                        ),
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 15.0, top: 5.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                  "${group.semYear} | ${group.owner}"),
-                            )),
-                        Padding(
-                            padding: const EdgeInsets.only(
-                                left: 15.0, top: 2.0),
-                            child: Align(
-                              alignment: Alignment.centerLeft,
-                              child: Text(
-                                  "${group.userIDs.length} people"),
-                            )),
-                        Padding(
-                            padding:
-                            const EdgeInsets.only(right: 15.0),
-                            child: Align(
-                              alignment: Alignment.bottomRight,
-                              child: TextButton(
-                                onPressed: () {},
-                                style: ButtonStyle(
-                                  backgroundColor:
-                                  MaterialStateProperty.all<
-                                      Color>(FlexColor.greenLightPrimary),
-                                  foregroundColor:
-                                  MaterialStateProperty.all<
-                                      Color>(Colors.white),
-                                ),
-                                child: const Text('Join'),
-                              ),
-                            )),
-                        const SizedBox(height: 10)
-                      ],
-                    ),
-                  ),
+              const Text(
+                "Their Courses:",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.left,
               ),
+              Column(
+                children: [
+                  ...groupCollection.getGroups(user.groupIDs).map(
+                        (group) => Card(
+                          elevation: 8,
+                          color: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                            side: BorderSide(
+                              color: Colors.grey.withOpacity(0.2),
+                              width: 1,
+                            ),
+                          ),
+                          child: Column(
+                            children: [
+                              ListTile(
+                                title: Text(group.groupName,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold)),
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15.0, top: 5.0),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                        "${group.semYear} | ${group.owner}"),
+                                  )),
+                              Padding(
+                                  padding: const EdgeInsets.only(
+                                      left: 15.0, top: 2.0),
+                                  child: Align(
+                                    alignment: Alignment.centerLeft,
+                                    child:
+                                        Text("${group.userIDs.length} people"),
+                                  )),
+                              Padding(
+                                  padding: const EdgeInsets.only(right: 15.0),
+                                  child: memberOfGroup(group.groupID)
+                                      ? Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: TextButton(
+                                            onPressed: () {
+                                              /// Remove the user from the group's database.
+                                              group.userIDs
+                                                  .remove(currentUser.uid);
+                                              currentUser.groupIDs
+                                                  .remove(group.groupID);
+                                              ref.refresh(allDataProvider);
+
+                                              /// Display "You have left the group" modal.
+                                              showDialog(
+                                                context: context,
+                                                builder: (BuildContext
+                                                        context) =>
+                                                    buildPopupDialog(context),
+                                              );
+                                            },
+                                            style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all<
+                                                      Color>(Colors.redAccent),
+                                              foregroundColor:
+                                                  MaterialStateProperty.all<
+                                                      Color>(Colors.white),
+                                            ),
+                                            child: const Text('LEAVE'),
+                                          ),
+                                        )
+                                      : Align(
+                                          alignment: Alignment.bottomRight,
+                                          child: TextButton(
+                                            onPressed: () {
+                                              /// Add the user to the group's database.
+                                              group.userIDs
+                                                  .add(currentUser.uid);
+                                              currentUser.groupIDs
+                                                  .add(group.groupID);
+                                              ref.refresh(allDataProvider);
+                                            },
+                                            style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all<
+                                                      Color>(Colors.green),
+                                              foregroundColor:
+                                                  MaterialStateProperty.all<
+                                                      Color>(Colors.white),
+                                            ),
+                                            child: const Text('JOIN'),
+                                          ),
+                                        )),
+                              const SizedBox(height: 10)
+                            ],
+                          ),
+                        ),
+                      ),
+                ],
+              )
             ],
-          )
-        ],
-      ),
-    );
+          ),
+        );
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(user.displayName),
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.message,
-                semanticLabel: 'message',
-              ),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => DirectMessageScreen(otherUser: user)));
-              },
+      appBar: AppBar(
+        title: Text(user.displayName),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.message,
+              semanticLabel: 'message',
             ),
-          ],
-        ),
-        body: ListView(
-          padding: const EdgeInsets.all(20.0),
-          children: [
-            Column(
-              children: [
-                ...displayInfo,
-                ...displayInterests,
-                displayGroups()
-              ],
-            ),
-          ],
-        ),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) =>
+                          DirectMessageScreen(otherUser: user)));
+            },
+          ),
+        ],
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(20.0),
+        children: [
+          Column(
+            children: [...displayInfo, ...displayInterests, displayGroups()],
+          ),
+        ],
+      ),
     );
   }
 }
